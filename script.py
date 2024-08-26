1d0aa42e7f56cad791ac41aaced45c0dc2aa73da
Joaquin Zubiri-PC <joaquinzubiri00@gmail.com>
2024-08-26 12:06:59 -0300

    merge done

--- script.py
+++ script.py
@@ -38,64 +38,69 @@ import mplfinance as mpl
 mpl.plot(df, type='candle', volume=True, mav=7)
 
 
-#Funcion para obtener datos historicos
-def get_historical_data(asset, timeframe, start, end):
-  data = client.get_historical_klines(asset, timeframe, start, end)
-  df = pd.DataFrame(data)
-  df=df.iloc[:,:6]
-  df.columns=["Date","Open","High","Low","Close","Volume"]
-  df=df.set_index("Date")
-  df.index=pd.to_datetime(df.index,unit="ms")
-  df=df.astype("float")
-  return df
+##### GRAFICAS MEJORADAS LUCHO
+
+# #Funcion para obtener datos historicos
+# def get_historical_data(asset, timeframe, start, end):
+#   data = client.get_historical_klines(asset, timeframe, start, end)
+#   df = pd.DataFrame(data)
+#   df=df.iloc[:,:6]
+#   df.columns=["Date","Open","High","Low","Close","Volume"]
+#   df=df.set_index("Date")
+#   df.index=pd.to_datetime(df.index,unit="ms")
+#   df=df.astype("float")
+#   return df
 
 
-#Obtener datos historicos de BTCUSDT
-asset="BTCUSDT"
+# #Obtener datos historicos de BTCUSDT
+# asset="BTCUSDT"
 
-start_5m="2024.07.20"
-end_5m="2024.07.27"
-data_5m = get_historical_data(asset, Client.KLINE_INTERVAL_5MINUTE, start_5m, end_5m)
+# start_5m="2024.07.20"
+# end_5m="2024.07.27"
+# data_5m = get_historical_data(asset, Client.KLINE_INTERVAL_5MINUTE, start_5m, end_5m)
 
-start_1h="2024.07.01"
-end_1h="2024.07.31"
-data_1h = get_historical_data(asset, Client.KLINE_INTERVAL_1HOUR, start_1h, end_1h)
-
-
-
-#Graficar datos con mplfinance
-mpl.plot(data_5m, type='candle', volume=True, mav=7, title="BTCUSDT - Intervalo de 5 minutos (MPL)")
-mpl.plot(data_1h, type='candle', volume=True, mav=7, title="BTCUSDT - Intervalo de 1 hora (MPL)")
+# start_1h="2024.07.01"
+# end_1h="2024.07.31"
+# data_1h = get_historical_data(asset, Client.KLINE_INTERVAL_1HOUR, start_1h, end_1h)
 
 
-#Graficar datos de cierre para comparar
-plt.figure(figsize=(12, 7))
-plt.subplot(2, 1, 1)
-plt.plot(data_5m["Close"], label="5 minutos")
-plt.title("BTCUSDT - Intervalo de 5 minutos (PLT)")
-plt.legend()
 
-plt.subplot(2, 1, 2)
-plt.plot(data_1h["Close"], label="1 hora")
-plt.title("BTCUSDT - Intervalo de 1 hora (PLT)")
-plt.legend()
-
-plt.tight_layout()
-plt.show()
+# #Graficar datos con mplfinance
+# mpl.plot(data_5m, type='candle', volume=True, mav=7, title="BTCUSDT - Intervalo de 5 minutos (MPL)")
+# mpl.plot(data_1h, type='candle', volume=True, mav=7, title="BTCUSDT - Intervalo de 1 hora (MPL)")
 
 
-#Calcular dimension fractales
-dim_5m = fractal_dimension(data_5m["Close"].values, 10)
-dim_1h = fractal_dimension(data_1h["Close"].values, 10)
+# #Graficar datos de cierre para comparar
+# plt.figure(figsize=(12, 7))
+# plt.subplot(2, 1, 1)
+# plt.plot(data_5m["Close"], label="5 minutos")
+# plt.title("BTCUSDT - Intervalo de 5 minutos (PLT)")
+# plt.legend()
 
-print(f"Dimension fractal (5 minutos): {dim_5m}")
-print(f"Dimension fractal (1 hora): {dim_1h}")
+# plt.subplot(2, 1, 2)
+# plt.plot(data_1h["Close"], label="1 hora")
+# plt.title("BTCUSDT - Intervalo de 1 hora (PLT)")
+# plt.legend()
+
+# plt.tight_layout()
+# plt.show()
 
 
-#Comparar las dimensiones fractales
-diff = dim_5m - dim_1h
+# #Calcular dimension fractales
+# dim_5m = fractal_dimension(data_5m["Close"].values, 10)
+# dim_1h = fractal_dimension(data_1h["Close"].values, 10)
 
-print(f"Diferencia de dimensiones fractales: {diff}")
+# print(f"Dimension fractal (5 minutos): {dim_5m}")
+# print(f"Dimension fractal (1 hora): {dim_1h}")
+
+
+# #Comparar las dimensiones fractales
+# diff = dim_5m - dim_1h
+
+# print(f"Diferencia de dimensiones fractales: {diff}")
+
+
+
 probAlcista = 0
 probBajista = 0
 probNeutra = 0
